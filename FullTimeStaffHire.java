79ded77df357e9a1f3ced93248bb9be3a88ea546
Sandesh Shahi <shahisandesh95@gmail.com>
2025-04-22 18:37:30 +0100

    Update FullTimeStaffHire.java

--- FullTimeStaffHire.java
+++ FullTimeStaffHire.java
@@ -1,5 +1,5 @@
-// FullTimeStaffHire.java
-public class FullTimeStaffHire extends StaffHire {
+public class FullTimeStaffHire extends StaffHire
+ {
     private double salary;
     private int weeklyFractionalHours;
     
@@ -7,41 +7,52 @@ public class FullTimeStaffHire extends StaffHire {
     public FullTimeStaffHire(int vacancyNumber, String designation, String jobType, 
                            String staffName, String joiningDate, String qualification, 
                            String appointedBy, boolean joined, double salary, 
-                           int weeklyFractionalHours) {
+                           int weeklyFractionalHours)
+// call parent class constructor  to set common attributes 
+{
         super(vacancyNumber, designation, jobType, staffName, joiningDate, 
               qualification, appointedBy, joined);
         this.salary = salary;
         this.weeklyFractionalHours = weeklyFractionalHours;
     }
     
-    // Getters and Setters
-    public double getSalary() {
+     //Accessor method ( Getters) and Mutator method (Setters)
+
+    public double getSalary()
+ {
         return salary;
     }
     
-    public int getWeeklyFractionalHours() {
+    public int getWeeklyFractionalHours() 
+{
         return weeklyFractionalHours;
     }
     
     // Method to set salary
-    public void setSalary(double newSalary) {
-        if (isJoined()) {
+    public void setSalary(double newSalary)
+ {
+        if (isJoined()) 
+{
             this.salary = newSalary;
-        } else {
+        } else 
+{
             System.out.println("Cannot set salary as no staff is appointed yet.");
         }
     }
     
     // Method to set weekly fractional hours
-    public void setWeeklyFractionalHours(int newWeeklyFractionalHours) {
+    public void setWeeklyFractionalHours(int newWeeklyFractionalHours) 
+{
         this.weeklyFractionalHours = newWeeklyFractionalHours;
     }
     
-    // Override display method
+    // Override display method of staffHire class
     @Override
-    public void display() {
+    public void display()
+ {
         super.display();
-        if (isJoined()) {
+        if (isJoined()) 
+{
             System.out.println("Salary: " + salary);
             System.out.println("Weekly Fractional Hours: " + weeklyFractionalHours);
         }
